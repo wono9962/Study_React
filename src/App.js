351726c0b8888828d381fae9658b72c3b6c95f4f
wono9962 <wono9962@naver.com>
2021-05-09 15:18:34 +0900

    modify: _title, _desc 값 넘겨와서 console에 출력하는거 까지 코드 추가

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      mode:'read',
+      mode:'create',
       selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -41,7 +41,10 @@ class App extends Component {
       }
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'create'){
-      _article = <CreateContent></CreateContent>
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        //add content to this.state.contents
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
     console.log('render', this);
     return (
